b26fd4ba65250d2152c3ee6d280e5821328bc7fb
joaoPedroC0elho <joaopcrsbsb@gmail.com>
2024-07-28 09:39:43 -0300

    Primeiro commmit do projeto

---
+++ testes/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+         int a, b;
+         a = 6;
+         b = 7;
+
+         String resultado = "";
+            if (a==b)
+                resultado = "verdadeiro";
+            else
+                resultado = "falso";
+
+        System.out.println(resultado);
+    }
+}
